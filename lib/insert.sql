fae7be77134eab91499008a3ba82346c889899e3
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-23 17:05:59 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Mr. Chocolate", 2, "M", "Brown", "Aloof", FALSE)
+  VALUES ("Mr. Chocolate", 2, "M", "Brown", "Aloof", FALSE);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Rowdy", 6, "NB", "Purple", "Standoffish", TRUE)
+  VALUES ("Rowdy", 6, "NB", "Purple", "Standoffish", TRUE);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Tabitha", 234, "F", "Pink", "Happy", TRUE)
+  VALUES ("Tabitha", 234, "F", "Pink", "Happy", TRUE);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Sergeant Brown", 8, "F", "Black", "Stalwart", FALSE)
+  VALUES ("Sergeant Brown", 8, "F", "Black", "Stalwart", FALSE);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Melissa", 12, "TF", "Black", "goofy", TRUE)
+  VALUES ("Melissa", 12, "TF", "Black", "goofy", TRUE);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Grinch", 6, "F", "Black", "Ironically Kind", TRUE)
+  VALUES ("Grinch", 6, "F", "Black", "Ironically Kind", TRUE);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Wendy", 3, "F", "Aquamarine", "There For You", TRUE)
+  VALUES ("Wendy", 3, "F", "Aquamarine", "There For You", TRUE);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES (NULL, 20, "M", "black", "aggressive", 0)
+  VALUES (NULL, 20, "M", "black", "aggressive", 0);
